d66a3b7e7abab8213716e2ba5f9634cf53f4d635
heySomil <avi.s2403@gmail.com>
2024-01-02 18:27:06 +0530

    second commit

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ function sendMessage(event) {
 
         // Clear the input field
         document.getElementById('userInput').value = '';
+
+        // Auto-scroll to the bottom
+        chatBody.scrollTop = chatBody.scrollHeight;
     }
 }
 
@@ -31,7 +34,10 @@ function handleChatLogic(userInput) {
     var response = getResponse(userInput);
 
     // Append the chatbot's response to the chat body
-    chatBody.innerHTML += '<img src="https://cdn-icons-png.flaticon.com/512/4712/4712035.png"></img>' + 'SciAstra Bot: ' + response ;
+    chatBody.innerHTML += '<img src="https://cdn-icons-png.flaticon.com/512/4712/4712035.png"></img>' + 'SciAstra Bot: ' + response;
+
+    // Auto-scroll to the bottom
+    chatBody.scrollTop = chatBody.scrollHeight;
 }
 
 function getResponse(userInput) {
@@ -42,12 +48,30 @@ function getResponse(userInput) {
             return "Hi there! How can I assist you?";
         case "services":
             return "SciAstra provides a range of services, including...";
+        case "courses":
+            return "SciAstra offers various courses, such as...";
         case "team":
             return "SciAstra's team consists of experts in...";
         case "contact":
             return "You can contact SciAstra at contact@sciastra.com.";
-        // Add more cases for other questions
+        case "selections":
+            return "SciAstra has an impressive track record of selections...";
+        case "merchandise":
+            return "SciAstra currently does offer merchandise for sale.";
+        case "recognition":
+            return "SciAstra is recognized in...";
+        case "jee and neet":
+            return "Yes, SciAstra provides coaching for JEE and NEET exams.";
+        case "app":
+            return "Yes, SciAstra has a dedicated mobile app for a better learning experience.";
+        case "students":
+            return "SciAstra has taught a significant number of students...";
+        case "selected":
+            return "Many students have been successfully selected through SciAstra.";
+        case "recorded classes":
+            return "Yes, SciAstra provides recorded classes for flexible learning.";
         default:
             return "I'm sorry, I didn't understand that. Please ask another question.";
     }
 }
+
